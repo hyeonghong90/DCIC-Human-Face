3bebaca92c030cab6078f6b2472636c658d2a2c7
Hyeong-gi <hyeonghong90@gmail.com>
2018-11-04 01:02:28 -0500

    Admin interface

--- html/admin_interface/edit.php
+++ html/admin_interface/edit.php
@@ -4,8 +4,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+	<!-- People name dropdown menu -->
 	<script src="js/edit_dropdown_names.js"></script>
 
+	<!-- Auto Complete Form CSS -->
 	<link rel="stylesheet" href="../css/auto_complete_form.css">
 
 	<!-- Bootstrap CSS -->
@@ -30,40 +32,41 @@
 
 	<div class="container">
 		<div class="row py-3" role="titlebar" id="titlebar">
-      		<div class="section-title"><h3>Parcel Information</h3></div>
-    	</div>
-    	<!-- Basic Parcel Information -->
-  		<div class="row rounded pb-5">
-  			<!--Iterate the parcel_info array -->
-  			<?php $i=0; foreach ($parcel_info[0] as $key => $value){ if($key == 'parcel_id'){continue;} ?>
-  			<div class="col-md-3">
+			<div class="section-title"><h3>Parcel Information</h3></div>
+    </div>
+
+    <!-- Basic Parcel Information -->
+		<div class="row rounded pb-5">
+			<!--Iterate the parcel_info array -->
+			<?php $i=0; foreach ($parcel_info[0] as $key => $value){ if($key == 'parcel_id'){continue;} ?>
+			<div class="col-md-3">
 				<label><?php echo $key;?><small class="required">*</small></label>
 				<?php if($key == 'block_no' || $key == 'parcel_no') { ?>
-    			<input type="text" class="parcel" id="<?php echo 'parcel' . $i;?>" parcel_id=<?php echo $parcel_info[0]['parcel_id'];?> name="<?php echo $key;?>" value = "<?php echo trim($value);?>" required minlength="1" onblur="pageUpdate(id)">
-    			<?php } else if ($key == 'ward_no') { ?>
-    			<input type="number" class="parcel" id="<?php echo 'parcel' . $i;?>" parcel_id=<?php echo $parcel_info[0]['parcel_id'];?> name="<?php echo $key;?>" value = "<?php echo trim($value);?>" onblur="pageUpdate(id)">
-    			<?php } else { ?>
-				<select class="form-control" id="<?php echo 'parcel' . $i;?>" parcel_id="<?php echo $parcel_info[0]['parcel_id'];?>" name="land_use" value="<?php echo trim($parcel_info[0]['land_use']);?>" onblur="pageUpdate(id)">
-					<option value="residential">Residential</option>
-					<option value="commercial">Commercial</option>
-				</select>
+	  			<input type="text" class="parcel" id="<?php echo 'parcel' . $i;?>" parcel_id=<?php echo $parcel_info[0]['parcel_id'];?> name="<?php echo $key;?>" value = "<?php echo trim($value);?>" required minlength="1" onblur="pageUpdate(id)">
+	  		<?php } else if ($key == 'ward_no') { ?>
+	  			<input type="number" class="parcel" id="<?php echo 'parcel' . $i;?>" parcel_id=<?php echo $parcel_info[0]['parcel_id'];?> name="<?php echo $key;?>" value = "<?php echo trim($value);?>" onblur="pageUpdate(id)">
+	  		<?php } else { ?>
+					<select class="form-control" id="<?php echo 'parcel' . $i;?>" parcel_id="<?php echo $parcel_info[0]['parcel_id'];?>" name="land_use" value="<?php echo trim($parcel_info[0]['land_use']);?>" onblur="pageUpdate(id)">
+						<option value="residential">Residential</option>
+						<option value="commercial">Commercial</option>
+					</select>
 				<?php } $i++; ?>
 			</div>
-			<?php } ?>
+		<?php } ?>
 
-  			<!-- Address Information -->
-        	<?php $index=0; for ($i=0; $i<sizeof($address_info); $i++) { foreach($address_info[$i] as $key => $value) { if ($key == 'address_id') {continue;}?>
-        	<div class="col-md-6">
-    			<label><?php echo trim($key);?></label>
+			<!-- Address Information -->
+	  	<?php $index=0; for ($i=0; $i<sizeof($address_info); $i++) { foreach($address_info[$i] as $key => $value) { if ($key == 'address_id') {continue;}?>
+	  	<div class="col-md-6">
+				<label><?php echo trim($key);?></label>
 				<input type="text" class="form-control" id="<?php echo 'address' . $index;?>" address_id="<?php echo $address_info[$i]['address_id'];?>" value = "<?php echo trim($value);?>" onblur="pageUpdate(id)">
-        	<?php $index++; ?>
-        	</div>
-        	<?php }} ?>
+	  	<?php $index++; ?>
+	  	</div>
+	  	<?php }} ?>
 		</div>
 
 		<div class="row py-3" role="titlebar" id="titlebar">
-      		<div class="section-title"><h3>Event Information</h3></div>
-    	</div>
+			<div class="section-title"><h3>Event Information</h3></div>
+    </div>
 
 		<!-- Event Information -->
 		<div class="container event-container px-0 pb-2">
@@ -104,12 +107,12 @@
 								<option value="End of Case" <?php if(trim($value) == 'End of Case'){ echo 'selected';}?>>End of Case</option>
 							</select>
 						</div>
-					<?php }	$index++; } 
+					<?php }	$index++; }
 					$people = people_names($event_info[$i]['event_id']);
 					for ($j=0; $j<sizeof($people); $j++) { ?>
 						<div class="col-md-12" id="<?php echo 'db_people' . $index_reverse;?>">
 							<div class="row">
-							<?php foreach ($people[$j] as $k => $v) { 
+							<?php foreach ($people[$j] as $k => $v) {
 								if ($k == 'event_asso_id' || $k == 'person_id') {
 									continue;
 								}
@@ -126,7 +129,7 @@
 										<option value="heir" <?php if(trim($v) == 'heir'){ echo 'selected';}?>>Heir</option>
 										<option value="appraiser" <?php if(trim($v) == 'appraiser'){ echo 'selected';}?>>Appraiser</option>
 										<option value="trustee" <?php if(trim($v) == 'trustee'){ echo 'selected';}?>>Trustee</option>
-									</select>	
+									</select>
 								</div>
 								<?php } elseif ($k == 'name') { ?>
 									<div class="col-md-7">
@@ -136,7 +139,8 @@
 							    		</div>
 							    	</div>
 							    	<div class="col-md-1 d-flex align-items-end justify-content-center">
-							    		<button class="btn btn-danger mb-1" onclick="deletePeople(this.parentNode.parentNode.parentNode.id, '<?php echo $people[$j]['event_asso_id'];?>')">
+							    		<button class="btn btn-danger mb-1" onclick="deletePeople(this.parentNode.parentNode.parentNode.id, this.parentNode.parentNode.children[1].children[2].children[0].id,
+											this.parentNode.parentNode.children[0].children[1].id)">
 							    			<i class="fas fa-trash-alt"></i>
 							    		</button>
 							    	</div>
@@ -153,17 +157,9 @@
 		</div>
 
 		<!-- Submit Button -->
-        <div class="col-md-12" role="submit-titlebar"  id="role-submit-titlebar">
-            <div class="section-title"><h3>Submit this Entry</h3></div>
-        </div>
-        <div class="col-md-12">    
-            <button onclick="updateDB();">SUBMIT</button>
-        </div>
-        	<div class="col-md-12 form-footer">            
-        </div>
-	    <div class="col-md-12">
-	    	<div class="required"> * Required</div>
-	    </div>
+    <div class="row pb-3">
+        <button class="btn btn-lg btn-outline-dark my-3" onclick="updateDB();">SUBMIT</button>
+    </div>
 	</div>
 
 	<br><br>
@@ -174,12 +170,11 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 
-<script src="js/edit_pageElements_update.js"></script>
+<script src="js/edit_page_elements_update.js"></script>
 <script src="js/edit_db_update.js"></script>
 <script src="js/edit_add_people.js"></script>
 <script src="js/edit_delete_people.js"></script>
 <script src="js/edit_dropdown_register.js"></script>
-<script src="js/edit_dropdown_names.js"></script>
 
 </body>
 </html>
